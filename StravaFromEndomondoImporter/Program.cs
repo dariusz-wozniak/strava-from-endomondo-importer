9aab98fa90120ff9901cd1dd56e53a4e90455ad4
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-07-20 18:54:36 +0200

    feat: add authentication (WIP)

--- StravaFromEndomondoImporter/Program.cs
+++ StravaFromEndomondoImporter/Program.cs
@@ -1,3 +1,14 @@
-﻿var options = Parser.Default.ParseArguments<Options>(args);
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var options = Parser.Default.ParseArguments<Options>(args).Value;
+
+var services = new ServiceCollection();
+
+services.AddAuthentication()
+        .AddStrava(o =>
+        {
+            o.ClientId = options.ClientId;
+            o.ClientSecret = options.ClientSecret;
+        });
 
 Console.ReadKey();
