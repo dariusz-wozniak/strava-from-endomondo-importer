285e09bf264862b7605f96ad033e13b22bd75106
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-08-13 01:43:04 +0200

    batch

--- StravaFromEndomondoImporter/Program.cs
+++ StravaFromEndomondoImporter/Program.cs
@@ -1,4 +1,5 @@
 ﻿var options = Parser.Default.ParseArguments<Options>(args).Value;
+
 var logger = Logging.Setup(options.Path);
 
 try
@@ -6,8 +7,7 @@ try
     if (options.SkipScanning) logger.Information("Skipping scanning");
     else await FileScanner.Scan(options, logger);
 
-    var (processed, total) = ActivitiesDataStore.GetStats(options);
-    logger.Information("Processed {Processed} of {Total} activities ({Percentage}%)", processed, total, (processed * 100) / total);
+    ShowStats(options, logger);
 
     var url = $"{AuthorizeUrl}?client_id={options.ClientId}&response_type={ResponseType}&redirect_uri={RedirectUri}&scope={Scope}";
     url = url.Replace("&", "^&");
@@ -17,18 +17,25 @@ try
     var code = Console.ReadLine()?.Trim() ?? string.Empty;
 
     var (accessToken, refreshToken) = await Strava.GetTokens(options, code);
+    
+    while (true)
+    {
+        // Upload:
+        var toBeUploaded = ActivitiesDataStore.GetActivities(options, Status.AddedToDataStoreWithDetails, take: Configuration.BatchSize);
+        logger.Information("Uploading {ActivitiesCount} activities", toBeUploaded.Count);
+        foreach (var activity in toBeUploaded) await Strava.UploadActivity(accessToken, activity, logger, options);
 
-    // TODO: add start of loop x batch here
+        // Update:
+        var toBeUpdated = ActivitiesDataStore.GetActivities(options, Status.UploadSuccessful, take: Configuration.BatchSize);
+        logger.Information("Updating {ActivitiesCount} activities", toBeUpdated.Count);
+        foreach (var activity in toBeUpdated) await Strava.UpdateActivity(accessToken, activity, logger, options);
 
-    // Uploading:
-    // var toBeUploaded = ActivitiesDataStore.GetActivities(options, Status.AddedToDataStoreWithDetails, take: Configuration.BatchSize);
-    // logger.Information("Uploading {ActivitiesCount} activities", toBeUploaded.Count);
-    // foreach (var activity in toBeUploaded) await Strava.UploadActivity(accessToken, activity, logger, options);
-
-    // Updating:
-    var toBeUpdated = ActivitiesDataStore.GetActivities(options, Status.UploadSuccessful, take: Configuration.BatchSize);
-    logger.Information("Updating {ActivitiesCount} activities", toBeUpdated.Count);
-    foreach (var activity in toBeUpdated) await Strava.UpdateActivity(accessToken, activity, logger, options);
+        if (!toBeUpdated.Any() && !toBeUploaded.Any())
+        {
+            logger.Information("PROCESSED ALL! 🎉 - No activities to upload or update");
+            ShowStats(options, logger);
+        }
+    }
 }
 catch (Exception e)
 {
@@ -37,24 +44,10 @@ catch (Exception e)
     Environment.Exit(0);
 }
 
-
-// for later:
-/*
-
-// TODO:
-// Get uploads
-// Update (e.g. gear)
-
-var rsPut = await apiHost.AppendPathSegments("activities", id)
-                      .WithOAuthBearerToken(accessToken)
-                      .AllowAnyHttpStatus()
-                      .PutJsonAsync(new
-                      {
-                          hide_from_home = true
-                      })
-                      .ReceiveString();
-
-
 Console.ReadKey();
 
-*/
+void ShowStats(Options o, Logger l)
+{
+    var (processed, total) = ActivitiesDataStore.GetStats(o);
+    l.Information("Processed {Processed} of {Total} activities ({Percentage}%)", processed, total, processed * 100 / total);
+}
