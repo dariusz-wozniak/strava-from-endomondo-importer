74b10ca91147ae7a52bcfb676207f4c062be44e3
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-08-14 12:36:23 +0200

    config: retry every 15 minutes

--- StravaFromEndomondoImporter/Infrastructure/Policies.cs
+++ StravaFromEndomondoImporter/Infrastructure/Policies.cs
@@ -8,7 +8,7 @@ public static class Policies
     {
         // Strava: The default rate limit allows 100 requests every 15 minutes, with up to 1,000 requests per day.
         var policy = Policy.Handle<FlurlHttpException>(e => e.StatusCode == (int)HttpStatusCode.TooManyRequests)
-                           .WaitAndRetryAsync(retryCount: RetryCount, retryAttempt => TimeSpan.FromMinutes(5),
+                           .WaitAndRetryAsync(retryCount: RetryCount, retryAttempt => TimeSpan.FromMinutes(15),
                                (exception, span) =>
                                {
                                    logger.Warning($"Retrying in {span.TotalMinutes} minutes... Exception: {exception?.Message}");
