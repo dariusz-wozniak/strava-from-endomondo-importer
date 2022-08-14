ca10a57b7badfb1bd07656a1ce26ea844f09cd17
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-08-14 17:04:11 +0200

    move files to the root

--- StravaFromEndomondoImporter/Configuration/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StravaFromEndomondoImporter.Configuration;
-
-public static class Config
-{
-    public static int BatchSizeForUploading => 80;
-    public static int BatchSizeForUpdating => 10;
-    public static int BatchSizeForEndomondoActivitySync => 10;
-    public static int RetryCount => 10;
-}
