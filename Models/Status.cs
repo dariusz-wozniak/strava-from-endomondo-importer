481c1a171b3fa1f2e07642dd146b40eeaa04a5e5
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-08-14 17:04:52 +0200

    add VCS files

---
+++ Models/Status.cs
@@ -0,0 +1,10 @@
+﻿namespace StravaFromEndomondoImporter.Models;
+
+public enum Status
+{
+    NotSet = 0,
+    StartedProcessingFile = 1,
+    AddedToDataStoreWithDetails = 2,
+    UploadSuccessful = 3,
+    UploadAndUpdateSuccessful = 4
+}
